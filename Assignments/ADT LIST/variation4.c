43d5b8cddcdd68d205b47f0ab9ecbf5b50792492
na638 <137698959+na638@users.noreply.github.com>
2025-09-16 20:30:39 +0800

    Update variation4.c

--- Assignments/ADT LIST/variation4.c
+++ Assignments/ADT LIST/variation4.c
@@ -1,62 +1,124 @@
 #include <stdio.h>
-#include <string.h>
-#define MAX 10
+#include <stdlib.h>
+
+#define LENGTH 5
 
 typedef struct {
-    char name[50];
-    char gender;
-    int age;
-    int next;
-} Node;
+    int *data;
+    int count;
+    int max;
+} List;
 
-typedef struct {
-    Node nodes[MAX];
-    int head;
-    int avail;
-} CursorList;
+void initialize(List *L);
+void insertPos(List *L, int data, int position);
+void deletePos(List *L, int position);
+int locate(List *L, int data);
+int retrieve(List *L, int position);
+void insertSorted(List *L, int data);
+void display(List *L);
+void resize(List *L);
+void makeNULL(List *L);
 
-void initialize(CursorList *L);
-int allocNode(CursorList *L);
-void freeNode(CursorList *L, int index);
-void insertFirst(CursorList *L, char name[], int age, char gender);
-void printList(CursorList L);
+int main() {
+    List L;
+    initialize(&L);
 
-void initialize(CursorList *L) {
-    int i;
-    for (i = 0; i < MAX - 1; i++) {
-        L->nodes[i].next = i + 1;
-    }
-    L->nodes[MAX - 1].next = -1;
-    L->avail = 0;
-    L->head = -1;
+    insertPos(&L, 10, 0);
+    insertPos(&L, 20, 1);
+    insertPos(&L, 30, 2);
+    insertPos(&L, 40, 3);
+    insertPos(&L, 50, 4);
+
+    display(&L);
+
+    deletePos(&L, 2);
+    display(&L);
+
+    printf("Locate 40: %d\n", locate(&L, 40));
+    printf("Retrieve at pos 1: %d\n", retrieve(&L, 1));
+
+    insertSorted(&L, 25);
+    display(&L);
+
+    makeNULL(&L);
+    return 0;
 }
 
-int allocNode(CursorList *L) {
-    if (L->avail == -1) return -1;
-    int index = L->avail;
-    L->avail = L->nodes[index].next;
-    return index;
-}
-
-void freeNode(CursorList *L, int index) {
-    L->nodes[index].next = L->avail;
-    L->avail = index;
-}
-
-void insertFirst(CursorList *L, char name[], int age, char gender) {
-    int index = allocNode(L);
-    if (index != -1) {
-        strcpy(L->nodes[index].name, name);
-        L->nodes[index].age = age;
-        L->nodes[index].gender = gender;
-        L->nodes[index].next = L->head;
-        L->head = index;
+void initialize(List *L) {
+    L->data = (int*)malloc(sizeof(int) * LENGTH);
+    L->count = 0;
+    L->max = LENGTH;
+    for (int i = 0; i < L->max; i++) {
+        L->data[i] = -1;
     }
 }
 
-void printList(CursorList L) {
-    if (L.head == -1) {
-        printf("-1\n");
-        return;
+void insertPos(List *L, int data, int position) {
+    if (position < 0 || position > L->count) return;
+    if (L->count == L->max) resize(L);
+    for (int i = L->count; i > position; i--) {
+        L->data[i] = L->data[i - 1];
     }
-    int curr = L
+    L->data[position] = data;
+    L->count++;
+}
+
+void deletePos(List *L, int position) {
+    if (position < 0 || position >= L->count) return;
+    for (int i = position; i < L->count - 1; i++) {
+        L->data[i] = L->data[i + 1];
+    }
+    L->data[L->count - 1] = -1;
+    L->count--;
+}
+
+int locate(List *L, int data) {
+    for (int i = 0; i < L->count; i++) {
+        if (L->data[i] == data) return i;
+    }
+    return -1;
+}
+
+int retrieve(List *L, int position) {
+    if (position < 0 || position >= L->count) return -1;
+    return L->data[position];
+}
+
+void insertSorted(List *L, int data) {
+    if (L->count == L->max) resize(L);
+    int i = L->count - 1;
+    while (i >= 0 && L->data[i] > data) {
+        L->data[i + 1] = L->data[i];
+        i--;
+    }
+    L->data[i + 1] = data;
+    L->count++;
+}
+
+void display(List *L) {
+    for (int i = 0; i < L->max; i++) {
+        printf("%d ", L->data[i]);
+    }
+    printf("\n");
+}
+
+void resize(List *L) {
+    int newMax = L->max * 2;
+    int *newData = (int*)malloc(sizeof(int) * newMax);
+    for (int i = 0; i < L->count; i++) {
+        newData[i] = L->data[i];
+    }
+    for (int i = L->count; i < newMax; i++) {
+        newData[i] = -1;
+    }
+    free(L->data);
+    L->data = newData;
+    L->max = newMax;
+}
+
+void makeNULL(List *L) {
+    free(L->data);
+    L->data = NULL;
+    L->count = 0;
+    L->max = 0;
+}
